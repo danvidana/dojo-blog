e8ae9e7d9dc4b81ad5ed46398fb483b5a4615451
danvidana <32367237+danvidana@users.noreply.github.com>
2022-04-06 17:40:10 -0500

    Modified App and created Navbar

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>
+                
+            </h1>
+        </nav>
+
+    );
+}
+ 
+export default Navbar;
