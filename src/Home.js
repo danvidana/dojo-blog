390724f274fb8c893e3e6922b4beefb8217f4dd0
danvidana <32367237+danvidana@users.noreply.github.com>
2022-04-20 17:03:31 -0500

    Up to lesson 11 props

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,17 @@
 import { useState } from "react";
-
+import BlogList from "./BlogList";
 
 const Home = () => {
     
-    //let name = "Mario";
-    const [name, setName] = useState("Mario");
-    const [age, setAge] = useState("25");
-
-    const handleClick = (e) => {
-        setName("Luigi");
-        setAge("30");
-    }
-
-    const handleClickAgain = (name, e) => {
-        console.log(name, e.target);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick={handleClick}>Click me</button>
-            {/*<button onClick={(e) => handleClickAgain("Yoshi", e)}>Click me again</button>*/}
+            <BlogList blogs={blogs} title="All Blogs" />
         </div>
     );
 }
